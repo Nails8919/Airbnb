5676e193e49ae528b6f1bbd55b8437f54c4a4b04
Lisa Mapp <218020739+Nails8919@users.noreply.github.com>
2026-03-06 18:44:48 -0400

    added formatting

--- readlisting.js
+++ readlisting.js
@@ -5,21 +5,40 @@ const readlistings = (res) => {
         .find(
             {},
             {
-                limit: 15,
-                // accommodation: 1,
-                // number_of_reviews: 1,
-                // review_scores: {review_scores_rating: 1},
-                // price: 1,
-                // _id : 1,
-                // summary: 1,
-                // host: {host_url: 1, host_name: 1, host_picture_url: 1},
+                limit: 17,
+                skip: 300,
+                sort: { number_of_reviews: -1 },
+                projection: {
+                    accommodates: 1,
+                    number_of_reviews: 1,
+                    review_scores: { review_scores_rating: 1 },
+                    price: 1,
+                    _id: 1,
+                    summary: 1,
+                    host: { host_url: 1, host_name: 1, host_picture_url: 1, host_is_superhost: 1 },
+                }
             })
         .toArray()
         .then(listings => {
             if (!listings)
                 res.status(404).json({ error: 'No listings found' })
-            else
+            else {
+                for (let doc of listings) {
+                    if (doc.price >= 1000) {
+                        let remainder = doc.price % 1000
+                        let priceS = Math.floor(doc.price - remainder) / 1000
+                        doc.price = `US$${priceS},${remainder}`
+                    }
+
+                    // console.log(doc.host.host_is_superhost)
+                    if (doc.host.host_is_superhost == true) {
+                        doc.host.host_name = `${doc.host.host_name} is a superhost`
+                    } else {
+                        doc.host.host_name = `${doc.host.host_name} is your host`
+                    }
+                }
                 res.json(listings)
+            }
         })
 }
 
