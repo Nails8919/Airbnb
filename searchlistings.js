6e7447d0cf73708e62789497a24bfa4ee055727f
Lisa Mapp <218020739+Nails8919@users.noreply.github.com>
2026-03-25 20:22:54 -0400

    Search

--- searchlistings.js
+++ searchlistings.js
@@ -2,33 +2,51 @@ import { listingsCollection } from "./mymongo.js"
 
 
 const searchListings = (res, query) => {
-    listingsCollection
-    .find({
-        minimum_nights: { $gte: query}
-        // maximum_nights: { $gte: 10 }
-    },
-     {
-        limit:10,
 
-        projection: {
-            _id: 1,
-            name: 1,
-            summary: 1,
-            address: 1,
-            property_type: 1,
-            reviews: 1,
-            score: 1,
-            rating: 1,
-            price: 1
-        }
-    })
-    .toArray()
-    .then(listing => {
-        if (!listing) {
-            res.status(404).json({ error: 'Listing not found' })    
-        }
-        res.json(listing)
-    })
+    const mongoQuery = {};
+    const numericValue = Number(query);
+
+
+    if (!isNaN(numericValue)) {
+        mongoQuery.minimum_nights = { $lte: numericValue };
+    }
+
+    else {
+        mongoQuery.property_type = {
+            $regex: `^${query}$`,
+            $options: "i" // case-insensitive
+        };
+    }
+    // if (query.minimum_nights) {
+    //     mongoQuery.minimum_nights = { $gte: query.minimum_nights };
+    // }
+
+    listingsCollection
+        .find(mongoQuery,
+            {
+                limit: 10,
+
+                projection: {
+                    _id: 1,
+                    name: 1,
+                    summary: 1,
+                    address: 1,
+                    property_type: 1,
+                    reviews: 1,
+                    score: 1,
+                    rating: 1,
+                    price: 1
+                }
+            })
+        .toArray()
+        .then(listing => {
+            if (!listing || listing.length === 0) {
+                res.status(404).json({ error: 'Listing not found' })
+            } else {
+                res.json(listing)
+            }
+            // res.json(listing)
+        })
 }
 
 export { searchListings }
